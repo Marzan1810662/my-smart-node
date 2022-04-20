e84e3cbc21fe1386f0d354638b549e7179076fcd
Marzan1810662 <hassanmarzan17@gmail.com>
2022-04-20 20:43:55 +0600

    steps added

---
+++ __steps.js
@@ -0,0 +1,27 @@
+/**
+ * -----------------------------
+ * basic server setup
+ * -----------------------------
+ * 1. create a folder (manually or mkdir)
+ * 2. open command line to that folder
+ * 3. mpn init
+ * 4. npm install express
+ * 5. create index.js
+ * ---------------------------------
+ * For Express
+ * ---------------------------------
+ * 1. require express
+ * 2. create app variable
+ * 3. declare port
+ * 4. set app.get('/')
+ * 5.listen to port
+ * 6. node index.js 
+ * 7. check your browswer for that port
+ * -------------
+ * get
+ * -------------
+ * 1. cors middleware
+ * ------------------------
+ * post
+ * ------------------------
+ */
